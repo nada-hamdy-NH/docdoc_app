e22dda75313d3a925ce43636b17344afdfe6c864
nada <nadahamdy09912@gmail.com>
2024-08-24 18:15:18 +0300

    just testing the connection the sourceTree connection with gitgub and project

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+s
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Docdoc App',
       theme: ThemeData(
         // This is the theme of your application.
         //
