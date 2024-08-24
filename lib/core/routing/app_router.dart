dbc20cf3f3f2c1bfeb7cd36ef7431700b6cae0c9
nada <nadahamdy09912@gmail.com>
2024-08-24 19:18:17 +0300

    dawnload packages bloc ,screenutil , svg , freezed , get_it ,dio , .... added images and svgs create onboarding screen create routing create doc_app mean file themeing empty login screen added navegation as extention

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,25 @@
+import'package:docdoc/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:docdoc/features/onbording/onbprding_screen.dart';
+import 'package:docdoc/features/login/login_screen.dart';
+class AppRouter{
+  Route generateRoute(RouteSettings settings){
+    //this argument is used to be passed in any screen like this(argument as class name)
+    final arguments = settings.arguments;
+    switch(settings.name){
+      case Routes.onBoardingScreen:
+      return MaterialPageRoute(
+        builder: (context) =>const OnBoardingScreen());
+        case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (context) =>const LoginScreen());
+        default:
+        return MaterialPageRoute(
+          builder: (_)=>Scaffold(
+            body: Center(child: Text("no route defined for${settings.name}")),
+            ));
+
+    }
+
+  }
+ }
