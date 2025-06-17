b1d82865e244a1ca3931227d2f3d2b479dd3c5d6
DESKTOP-VD4OAC8\m <nadahamdy09912@gmail.com>
2025-06-17 23:52:50 +0200

    added saveSecuredToken

--- lib/core/helper/extensions.dart
+++ lib/core/helper/extensions.dart
@@ -14,5 +14,9 @@ extension Navegation on BuildContext {
       routeName, predicate, arguments: arguments);
   }
   void pop() => Navigator.of(this).pop();
+}
+
+  extension  StringExtension on String? {
+    bool isNullOrEmpty() => this == null || this=="";
   
 }
